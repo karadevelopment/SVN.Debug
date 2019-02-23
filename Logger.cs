05f1d0b330c4c1de152ff04b0b4a116a3c403b01
Muhammed Kara <47905966+svndevelopment@users.noreply.github.com>
2019-02-23 01:36:03 +0100

    1.0.3.0

---
+++ Logger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace SVN.Debug
+{
+    public static class Logger
+    {
+        public static int Limit { get; set; } = 10000;
+        public static int Buffer { get; set; } = 2000;
+
+        public static void Reset()
+        {
+            Resource.Default.Log = string.Empty;
+            Resource.Default.Save();
+        }
+
+        private static void LimitLines()
+        {
+            var lines = Resource.Default.Log.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+
+            if (Logger.Limit < lines.Count())
+            {
+                lines.RemoveRange(0, Logger.Buffer);
+                Resource.Default.Log = string.Join(Environment.NewLine, lines);
+            }
+        }
+
+        public static void Write(params object[] messages)
+        {
+            Logger.LimitLines();
+
+            Resource.Default.Log += Environment.NewLine;
+            Resource.Default.Log += DateTime.Now;
+            Resource.Default.Log += Environment.NewLine;
+            Resource.Default.Log += Assembly.GetCallingAssembly();
+            Resource.Default.Log += Environment.NewLine;
+
+            foreach (var message in messages)
+            {
+                Resource.Default.Log += message;
+                Resource.Default.Log += Environment.NewLine;
+            }
+
+            Resource.Default.Save();
+        }
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace SVN.Debug
+{
+    public class Timer
+    {
+        private Stopwatch Stopwatch { get; } = new Stopwatch();
+
+        private Timer()
+        {
+        }
+
+        public static Timer Start()
+        {
+            var result = new Timer();
+            result.Restart();
+            return result;
+        }
+
+        public string Restart()
+        {
+            var result = this.Stop();
+            this.Stopwatch.Restart();
+            return result;
+        }
+
+        public string Stop()
+        {
+            this.Stopwatch.Stop();
+            return this.ToString();
+        }
+
+        public override string ToString()
+        {
+            var time = this.Stopwatch.Elapsed;
+            return $"{time.Hours:D2}:{time.Minutes:D2}:{time.Seconds:D2}.{time.Milliseconds:D3}";
+        }
+    }
+}
